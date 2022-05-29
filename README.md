b8cd49428258fd7aeaf3f6a401844060f6133ba8
Teyhan USLU <teyhan.uslu@accenture.com>
2022-05-29 21:53:07 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MVVMBaseProtocols
+
+A description of this package.
---
+++ Sources/MVVMBaseProtocols/MVVMBaseProtocols.swift
@@ -0,0 +1,6 @@
+public struct MVVMBaseProtocols {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
